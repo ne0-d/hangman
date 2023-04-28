2e5435c53d4c979f0a8eda41c627f24d43582a31
ne0-d <dhruvbhati2001@gmail.com>
2023-04-29 04:43:22 +0530

    another fix

--- frontend/src/components/Sharable/SharePage.jsx
+++ frontend/src/components/Sharable/SharePage.jsx
@@ -4,7 +4,7 @@ import {AES, enc} from "crypto-js"
 
 
 const SharePage = () => {
-    let baseShare = "https://hangman-app.up.railway.app/user";
+    let baseShare = "https://hangman-app.up.railway.app/sharable/";
     const [copy, setCopy] = useState(baseShare);
     const [copyinp, setcopyinp] = useState("");
     // var ciphertext = AES.encrypt(JSON.stringify(copyinp), 'my-secret-key@123').toString();
